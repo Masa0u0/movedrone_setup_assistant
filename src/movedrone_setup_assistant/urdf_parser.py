fc77386dc881d62f79a132999b470492872a3d89
Masa0u0 <masa0u0masa1215@gmail.com>
2023-02-04 16:27:34 +0900

    .

--- src/movedrone_setup_assistant/urdf_parser.py
+++ src/movedrone_setup_assistant/urdf_parser.py
@@ -7,7 +7,7 @@ from typing import List
 from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
-from urdf_parser_py.urdf import URDF, Link, Joint  # https://github.com/ros/urdf_parser_py
+from urdf_parser_py.urdf import Robot, Link, Joint  # https://github.com/ros/urdf_parser_py
 
 
 class URDFParser(QWidget):
@@ -18,31 +18,30 @@ class URDFParser(QWidget):
         super().__init__()
         self.main = main
 
-        self.urdf = None
-        self.tree = None
+        self.robot = Robot()
 
     def define_connections(self) -> None:
         self.main.settings.start.robot_model_loader.urdf_loaded.connect(self._on_urdf_loaded)
 
     @pyqtSlot()
     def _on_urdf_loaded(self) -> None:
-        self.urdf = URDF.from_parameter_server("/robot_description")
+        self.robot = Robot.from_parameter_server("/robot_description")
         self.robot_model_updated.emit()
 
     def get_links(self) -> List[Link]:
-        return self.urdf.links
+        return self.robot.links
 
     def get_joints(self) -> List[Joint]:
-        return self.urdf.joints
+        return self.robot.joints
 
     def get_root(self) -> Link:
-        root_name = self.urdf.get_root()
-        return self.urdf.link_map[root_name]
+        root_name = self.robot.get_root()
+        return self.robot.link_map[root_name]
 
     def get_joint(self, link_name: str) -> Joint:
-        joint_name, _ = self.urdf.parent_map[link_name]
-        return self.urdf.joint_map[joint_name]
+        joint_name, _ = self.robot.parent_map[link_name]
+        return self.robot.joint_map[joint_name]
 
     def get_parent(self, link_name: str) -> Link:
-        _, parent_name = self.urdf.parent_map[link_name]
-        return self.urdf.link_map[parent_name]
+        _, parent_name = self.robot.parent_map[link_name]
+        return self.robot.link_map[parent_name]

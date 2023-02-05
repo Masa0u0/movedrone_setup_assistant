3486cb8051d419b698bd89b472262d9f4abde756
Masa0u0 <masa0u0masa1215@gmail.com>
2023-02-05 14:14:37 +0900

    .

--- src/movedrone_setup_assistant/setting_widgets/ros_package.py
+++ src/movedrone_setup_assistant/setting_widgets/ros_package.py
@@ -7,7 +7,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
-from ..setting_widgets.base_setting import BaseSettingWidget
+from .base_setting import BaseSettingWidget
 
 
 class RosPackageWidget(BaseSettingWidget):
--- src/movedrone_setup_assistant/setting_widgets/start.py
+++ src/movedrone_setup_assistant/setting_widgets/start.py
@@ -12,7 +12,7 @@ from PyQt5.QtCore import *
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 
-from ..setting_widgets.base_setting import BaseSettingWidget
+from .base_setting import BaseSettingWidget
 from ..utils import get_pkg_path
 from ..const import *
 
